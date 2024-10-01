af0cb28749eb8e2b55f3f67c75e6ae4ba9372899
Alif Wibowo <alif.likes.coding@gmail.com>
2024-10-01 15:22:35 +0700

    Basic Setup

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+			fonts: {
+				ubuntu: ["Ubuntu", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+			},
+      colors: {
+        customRed : "#FE4342",
+				customBlue : "#5F96F7",
+				customPink : "#E86AE1",
+				customCream : "#FDF4EE",
+				customWhite : "#F9F5F2",
+				customBlack : "#282825"
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate],
+};
+
+export default config;
