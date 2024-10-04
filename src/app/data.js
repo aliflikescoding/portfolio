1b3205f2cc5cb0642ec16571d59d8b428350ec2d
Alif Wibowo <alif.likes.coding@gmail.com>
2024-10-05 02:53:06 +0700

    Finish All Projects

--- src/app/data.js
+++ src/app/data.js
@@ -19,7 +19,7 @@ export const freeLanceProjects = [
     slugLink: "#",
   },
   {
-    id: 1,
+    id: 2,
     title: "University Class Bot Booker",
     used: [
       "Figma",
@@ -50,6 +50,7 @@ export const freeLanceProjects = [
   },
 ];
 
+// cnt 7
 export const personalOrCourse = [
   {
     id: 0,
@@ -60,6 +61,149 @@ export const personalOrCourse = [
     imageLink: "/project-thumbnails/shoppingcartpict.png",
     liveLink: "https://shopping-cart-project-inky.vercel.app/",
     codeLink: "https://github.com/aliflikescoding/react-shopping-cart",
+    slugLink: "#",
+  },
+  {
+    id: 3,
+    title: "Memory Card Game",
+    used: ["react", "tailwindCSS", "reactRouting", "Framer Motion"],
+    description:
+      "An assigned project from The Odin Project in this project you are tasked to make a memory card game in ReactJS and pulling the image of the card from an API. In this project I learned about pulling data from an API in ReactJS, the useEffect hook in react and framer motion animation, and tailwind CSS",
+    imageLink: "/project-thumbnails/memorycardgame.png",
+    liveLink: "https://memory-card-project-dusky.vercel.app/",
+    codeLink: "https://github.com/aliflikescoding/react-memory-card",
+    slugLink: "#",
+  },
+  {
+    id: 4,
+    title: "CV Application",
+    used: ["react", "SASS"],
+    description:
+      "An asinged project in The Odin Project Course. It's a CV making application using ReactJS. In this project I learned about basic react principles, basic react rendering, and the useState react hook",
+    imageLink: "/project-thumbnails/cvappreact.png",
+    liveLink: "https://cv-application-project.vercel.app/",
+    codeLink: "https://github.com/aliflikescoding/react-cv-application",
+    slugLink: "#",
+  },
+  {
+    id: 5,
+    title: "Clone Nike Website",
+    used: ["react", "tailwindCSS"],
+    description:
+      "A tutorial project that I learned from youtube in this project I learned the basics for me to be able to use tailwind CSS",
+    imageLink: "/project-thumbnails/clonenik.png",
+    liveLink: "https://clone-nike-website-project.vercel.app/",
+    codeLink: "https://github.com/aliflikescoding/react-clone-nike-website",
+    slugLink: "#",
+  },
+  {
+    id: 1,
+    title: "Team App - Landing Page",
+    used: ["Webflow", "Web Design"],
+    description:
+      "This is a homepage design and build for a concept project – a chat application. I designed the page first then built a responsive web page using Webflow.",
+    imageLink: "/project-thumbnails/webflowchatapp.png",
+    liveLink: "https://aliflikescoding-team-app.webflow.io/",
+    codeLink: "#",
+    slugLink: "#",
+  },
+  {
+    id: 2,
+    title: "Chat App - Landing Page",
+    used: ["Webflow", "Web Design"],
+    description:
+      "An assigned project from the udemy course complete webdesign by Vako Shvili. In this project you are tasked from converting your previous figma landing page file into actual code by using webflow. In this project I learned the basics of webflow and a lot about webdesign, and opened my eyes to a lot more css techniques that I did not know of before.",
+    imageLink: "/project-thumbnails/chatapplandingpage.png",
+    liveLink: "https://aliflikescoding-chatapp-landingpage.webflow.io/",
+    codeLink: "https://github.com/aliflikescoding/webflow-chatapp-landing-page",
+    slugLink: "#",
+  },
+  {
+    id: 6,
+    title: "To Do List - Landing Page",
+    used: ["HTML", "SASS", "javascript"],
+    description:
+      "I learned SASS by making this landing page project for an existing project that I have made in the past that is the to do list app from the odin project",
+    imageLink: "/project-thumbnails/todolanding.png",
+    liveLink: "https://aliflikescoding.github.io/vanilla-to-do-list-landing-page/",
+    codeLink: "https://github.com/aliflikescoding/vanilla-to-do-list-landing-page",
+    slugLink: "#",
+  },
+  {
+    id: 7,
+    title: "Vanilla Battleship",
+    used: ["HTML", "SASS", "Javascript", "JEST"],
+    description:
+      "An assigned project from The Odin Project in this project you are assgined to make a clasic battleship game using html, css, and javasript but I used sass isntead. you arer assigned to use OOP to make the ships and JEST to test the project. In this project I learned about advaced OOP in javascript unit testing javascript logic using JEST",
+    imageLink: "/project-thumbnails/battleship.png",
+    liveLink: "https://aliflikescoding.github.io/vanilla-battleship/",
+    codeLink: "https://github.com/aliflikescoding/vanilla-battleship",
+    slugLink: "#",
+  },
+  {
+    id: 8,
+    title: "Vanilla Sign-Up From",
+    used: ["HTML", "SASS"],
+    description:
+      "An assigned project from TOP (the odin project) course. You are tasked to build a To Do List App using vanilla html, css, and Javasript. In this project I learend about OOP intensively in javascript",
+    imageLink: "/project-thumbnails/signupform.png",
+    liveLink: "https://aliflikescoding.github.io/vanilla-to-do-list/",
+    codeLink: "https://aliflikescoding.github.io/vanilla-to-do-list/",
+    slugLink: "#",
+  },
+  {
+    id: 9,
+    title: "Vanilla Weather App",
+    used: ["HTML", "CSS", "javascript"],
+    description:
+      "An assigned project from TOP (the odin project) course. You are tasked to build a weather app project by getting the weather data from an api. in this project I learned about getting data from an api",
+    imageLink: "/project-thumbnails/weatherapp.png",
+    liveLink: "https://aliflikescoding.github.io/vanilla-weather-app/",
+    codeLink: "https://github.com/aliflikescoding/vanilla-weather-app",
+    slugLink: "#",
+  },
+  {
+    id: 10,
+    title: "Vanilla Restaurant Page",
+    used: ["HTML", "CSS", "javascript"],
+    description:
+      'An assigned project from TOP (the odin project) course. You are tasked to build a restaurant page with tabbed browsing using the webpack package manager. In this project I learned basic webpack, dom manipulation, css animations, javascript tabbed browing, exporting/importing "modules"',
+    imageLink: "/project-thumbnails/restaurantpage.png",
+    liveLink: "https://aliflikescoding.github.io/vanilla-restaurant-page/",
+    codeLink: "https://github.com/aliflikescoding/vanilla-restaurant-page",
+    slugLink: "#",
+  },
+  {
+    id: 11,
+    title: "CSS Admin Dashboard",
+    used: ["HTML", "CSS", "Javascript"],
+    description:
+      "An asigned project from the odin project course. you are told to build a tic tac toe game in vanilla html, css, and javascript with a bot which is optional and make it with either modules or factory functions. I learned Object creation with factory functions, Basic Module constructions, DOM manipulation, Array manipulation, Object manipulation, JavaScript Event Listeners, Javascript time manipulation, Getting basic form data, Changing default actions, CSS animations",
+    imageLink: "/project-thumbnails/tictactoe.png",
+    liveLink: "https://aliflikescoding.github.io/vanilla-tic-tac-toe/",
+    codeLink: "https://github.com/aliflikescoding/vanilla-tic-tac-toe",
+    slugLink: "#",
+  },
+  {
+    id: 12,
+    title: "Vanilla Etch A Sketch",
+    used: ["HTML", "CSS", "Javascript"],
+    description:
+      "An asinged project in The Odin Project Course. It's an etch a sketch where you can draw stuff on them in this project I learned the basic skills of dom manipulation/selection and css",
+    imageLink: "/project-thumbnails/etchasketch.png",
+    liveLink: "https://aliflikescoding.github.io/vanilla-etch-a-sketch/",
+    codeLink: "https://github.com/aliflikescoding/vanilla-etch-a-sketch",
+    slugLink: "#",
+  },
+  {
+    id: 13,
+    title: "Vanilla Rock, Scissors, & Scisor",
+    used: ["HTML", "CSS", "Javascript"],
+    description:
+      "An asinged project in The Odin Project. It's just basic html, css, and javascript this is the first project that got me introduced into git, github, and vanilla html, css and javascript, a basic rock paper scisors but I added a twist I happen to know a game similar to Rock Paper Scissors from Indonesia called suten so I decided to make that instead of rock paper scissors but the logic is still the same.",
+    imageLink: "/project-thumbnails/suten.png",
+    liveLink: "https://aliflikescoding.github.io/vanilla-rock-paper-scissors/",
+    codeLink: "https://github.com/aliflikescoding/vanilla-rock-paper-scissors",
     slugLink: "#",
   },
 ];
@@ -119,7 +263,7 @@ export const featuredProjects = [
   },
   {
     id: 4,
-    title: "Chat App - Landing Page",
+    title: "Team App - Landing Page",
     used: ["Webflow", "Web Design"],
     description:
       "This is a homepage design and build for a concept project – a chat application. I designed the page first then built a responsive web page using Webflow.",
