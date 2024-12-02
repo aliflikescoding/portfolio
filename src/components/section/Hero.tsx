7dc248f70a7b8d65b6bd18acbd188e1009dfddee
Alif Wibowo <alif.likes.coding@gmail.com>
2024-12-02 15:23:45 +0700

    Chane resume name

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -100,7 +100,7 @@ const Hero = () => {
               <Link href="mailto:alif.likes.coding@gmail.com">
                 <ButtonComponent type="primary">get in touch</ButtonComponent>
               </Link>
-              <Link href="/resume.pdf" target="_" download="Alif Wibowo Resume">
+              <Link href="/Alif-Resume.pdf" target="_" download="Alif Wibowo Resume">
                 <ButtonComponent type="secondary">get resume</ButtonComponent>
               </Link>
             </motion.div>
